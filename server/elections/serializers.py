d27e181051f127f9b16496f2ad7b9834bfd95c5c
magayevenire <magayendiaye56@gmail.com>
2022-10-22 23:36:28 +0000

    periode

--- server/elections/serializers.py
+++ server/elections/serializers.py
@@ -1,16 +1,11 @@
 from rest_framework import serializers
-from .models import Election ,Periode 
+from .models import Election 
 
-class PeriodeSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Periode
-        fields = ("__all__")
-        
+
 class ElectionSerializer(serializers.ModelSerializer):
-    periode_inscription =  PeriodeSerializer(many=False,)
-    periode_depot_canditature =  PeriodeSerializer(many=False,)
+
     
     class Meta:
         model = Election
         fields = ("__all__")
+
