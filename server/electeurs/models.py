41bbbcc4f811febfaebc837afd43dcce59e59b4a
magayevenire <magayendiaye56@gmail.com>
2022-10-23 11:09:04 +0000

    related name

--- server/electeurs/models.py
+++ server/electeurs/models.py
@@ -21,6 +21,9 @@ class Electeur(models.Model):
 
     creation = models.DateTimeField(auto_now_add=True)
     modifier = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.prenom} -  {self.nom}"
 
 
 # class parrainage(models.Model):
@@ -39,6 +42,9 @@ class Candidature(models.Model):
     creation = models.DateTimeField(auto_now_add=True)
     modifier = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.nom_parti} -  {self.candidat.prenom} -  {self.candidat.nom}"
+
 
 
 class ElecteurVote(models.Model):
@@ -50,6 +56,9 @@ class ElecteurVote(models.Model):
 
     creation = models.DateTimeField(auto_now_add=True)
     modifier = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.eleteur} - {self.election}"
 class Vote(models.Model):
 
     # electeur = models.ForeignKey(Electeur,related_name='canditatues', on_delete=models.CASCADE,null=True,blank=True)
@@ -57,3 +66,6 @@ class Vote(models.Model):
     canditaure = models.ForeignKey(Candidature, on_delete=models.CASCADE,null=True,blank=True)
     creation = models.DateTimeField(auto_now_add=True)
     modifier = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.canditaure}"
